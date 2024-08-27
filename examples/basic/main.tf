808c37171b2ff178e48afa4bb2081a57669d23e4
Volodymyr S. <26582191+SweetOps@users.noreply.github.com>
2024-08-27 10:37:05 -0400

    ci: overhaul workflows (#22)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,12 @@
 module "aweasome_module" {
-  source    = "../../"
-  name      = "aweasome"
-  stage     = "production"
-  namespace = "sweetops"
+  source     = "../../"
+  name       = "aweasome"
+  stage      = "production"
+  namespace  = "sweetops"
+  attributes = var.attributes
+}
+
+output "id" {
+  value       = module.aweasome_module.id
+  description = "ID"
 }
---
+++ examples/basic/variables.tf
@@ -0,0 +1,10 @@
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = <<-EOT
+    ID element. Additional attributes (e.g. `workers` or `cluster`) to add to `id`,
+    in the order they appear in the list. New attributes are appended to the
+    end of the list. The elements of the list are joined by the `delimiter`
+    and treated as a single ID element.
+    EOT
+}
